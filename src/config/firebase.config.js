48cc7082000d31003a35c36c07ad7512172b19e7
Aditi-2 <aditig734@gmail.com>
2019-05-01 20:48:41 +0530

    update firebase config

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,10 +2,16 @@
  * Firebase API Details
  *
  * Note: below details are fake and most probably will not work
- * I too them from https: //vuejs.org/v2/examples/firebase.html
+ * I took them from https: //vuejs.org/v2/examples/firebase.html
+ *
+ * Follow below steps to get yours
+ * https: //firebase.google.com/docs/web/setup?authuser=0#obtain-config-object
  */
 export default {
   apiKey: 'AIzaSyAi_yuJciPXLFr_PYPeU3eTvtXf8jbJ8zw',
   authDomain: 'vue-demo-537e6.firebaseapp.com',
-  databaseURL: 'https://vue-demo-537e6.firebaseio.com'
+  databaseURL: 'https://vue-demo-537e6.firebaseio.com',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
 }
