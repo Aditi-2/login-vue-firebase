9efb3d3d76dec262bb13cd0230043420fed41853
Aditi-2 <aditig734@gmail.com>
2019-05-03 18:10:35 +0530

    added header component

---
+++ src/components/header.vue
@@ -0,0 +1,11 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Vue + Firebase Login</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+  </b-navbar>
+</template>
+<script>
+export default {
+  name: 'HeaderComponent'
+}
+</script>
